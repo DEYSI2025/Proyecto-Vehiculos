2dd98d1a7e0e5301602b52f1d99c93ef827354c5
DEYSI2025 <DEYSINELLY.QUISPE@GMAIL.COM>
2026-03-06 22:43:30 -0500

    Creacion de clase auto que hereda de Vehiculo

---
+++ src/Vehiculos/Auto.java
@@ -0,0 +1,16 @@
+package Vehiculos;
+
+public class Auto extends Vehiculo {
+
+    private int puertas;
+
+    public Auto(String marca, int velocidad, int puertas) {
+        super(marca, velocidad);
+        this.puertas = puertas;
+    }
+
+    @Override
+    public void mover() {
+        System.out.println("El auto se mueve por la carretera");
+    }
+}
